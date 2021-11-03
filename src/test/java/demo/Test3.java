0d81b7c15a725d56c42be2db3445f3822fc90e58
QSP <QSP@QSP>
2021-11-03 15:21:57 +0530

    hello

--- src/test/java/demo/Test3.java
+++ src/test/java/demo/Test3.java
@@ -1,10 +1,9 @@
 package demo;
 
-public class Test1 {
+public class Test3 {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-		
+		System.out.println("hello python");
 	}
 
 }
